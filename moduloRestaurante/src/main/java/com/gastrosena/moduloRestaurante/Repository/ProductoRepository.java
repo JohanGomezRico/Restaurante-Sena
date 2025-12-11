0225016de3691118b729164f750551ebbf6e0133
Aprendiz <nicolascarmona1306@gmail.com>
2025-12-11 14:44:49 -0500

    Tercer commit no se guardó el repository

--- moduloRestaurante/src/main/java/com/gastrosena/moduloRestaurante/Repository/ProductoRepository.java
+++ moduloRestaurante/src/main/java/com/gastrosena/moduloRestaurante/Repository/ProductoRepository.java
@@ -1,5 +1,10 @@
 package com.gastrosena.moduloRestaurante.Repository;
 
-public interface ProductoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gastrosena.moduloRestaurante.Entity.Pedido;
+import com.gastrosena.moduloRestaurante.Entity.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
 }
